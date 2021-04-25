7dae50f00aea3246d8333acc1a16fbadfdbacb9f
jamil-source <jamilbariche9@gmail.com>
2021-04-25 14:55:46 +0200

    create location search and fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import WeatherCard from './components/weathercard';
+import { API_URL, API_KEY } from './apis/config';
+import UseFetch from './hooks/fetch_location';
+import LocationSelector from './components/locationSelect';
+import {Container} from 'react-bootstrap';
+
 
 function App() {
+  const {data, error, isLoading, setUrl} = UseFetch();
   return (
-    <div className="App">
-      
-    </div>
+    <Container className="App">
+      <LocationSelector onSearch = {(city) => setUrl(`${API_URL}data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`)}/>
+    </Container>
   );
 }
 
